1fab7e98831c206d47090348ceaf673cad0e4b32
arantruslove <125707012+arantruslove@users.noreply.github.com>
2024-06-19 21:00:14 +0100

    Dynamic activation of nav tabs based on url.

--- frontend/src/components/MainNav.jsx
+++ frontend/src/components/MainNav.jsx
@@ -1,14 +1,59 @@
 import { Nav } from "react-bootstrap";
+import { useLocation, useNavigate } from "react-router-dom";
 
 function MainNav() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
   return (
-    <Nav className="flex-column" variant="pills">
-      <Nav.Link href="#home">Schedule</Nav.Link>
-      <Nav.Link href="#link1">Create Schedule</Nav.Link>
-      <Nav.Link href="#link2">All Courses</Nav.Link>
-      <Nav.Link href="#link3">Course Search</Nav.Link>
-      <Nav.Link href="#disabled">Create Course</Nav.Link>
-      <Nav.Link href="/account">Account</Nav.Link>
+    <Nav
+      className="flex-column"
+      variant="pills"
+      activeKey={location["pathname"]}
+    >
+      <Nav.Link
+        eventKey="/home"
+        onClick={() => {
+          navigate("/home");
+        }}
+      >
+        Home
+      </Nav.Link>
+      <Nav.Link
+        eventKey=""
+        onClick={() => {
+          navigate("/create-schedule");
+        }}
+      >
+        Create Schedule
+      </Nav.Link>
+      <Nav.Link
+        eventKey="/all-courses"
+        onClick={() => {
+          navigate("/all-courses");
+        }}
+      >
+        All Courses
+      </Nav.Link>
+      <Nav.Link
+        eventKey="/course-search"
+        onClick={() => {
+          navigate("/course-search");
+        }}
+      >
+        Course Search
+      </Nav.Link>
+      <Nav.Link
+        eventKey="/create-course"
+        onClick={() => {
+          navigate("/create-course");
+        }}
+      >
+        Create Course
+      </Nav.Link>
+      <Nav.Link eventKey="/account" onClick={() => navigate("/account")}>
+        Account
+      </Nav.Link>
     </Nav>
   );
 }
