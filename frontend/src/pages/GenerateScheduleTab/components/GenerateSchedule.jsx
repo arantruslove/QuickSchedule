ebd98bcbbb581b2034d9d07fbc9f2f8ae93da647
arantruslove <125707012+arantruslove@users.noreply.github.com>
2024-06-23 12:05:26 +0100

    Refactoring such that all server interactions specific to AllocateHours components are within those components and not in the GeneratingSchedule component.

--- frontend/src/pages/GenerateScheduleTab/components/GenerateSchedule.jsx
+++ frontend/src/pages/GenerateScheduleTab/components/GenerateSchedule.jsx
@@ -1,37 +1,13 @@
-import { useState, useEffect } from "react";
-import { Card, Nav, Spinner } from "react-bootstrap";
+import { useState } from "react";
+import { Card, Nav } from "react-bootstrap";
 
-import {
-  createFormDraft,
-  getFormDraft,
-} from "../../../services/scheduleRequests";
 import AllocateHoursContainer from "../containers/AllocateHoursContainer";
 import SelectPlans from "./SelectPlans";
 import ScheduleInformation from "./ScheduleInformation";
 import MakeAdjustments from "./MakeAdjustments";
 
 function GenerateSchedule() {
-  const [isLoading, setIsLoading] = useState(true);
   const [tabNumber, setTabNumber] = useState(1);
-  const [formDraftData, setFormDraftData] = useState(null);
-
-  const updatePageData = async () => {
-    let response = await getFormDraft();
-
-    // Create a ScheduleFormDraft instance if it does not already exist
-    if (response.status === 404) {
-      response = await createFormDraft();
-    }
-
-    if (response.ok) {
-      const data = await response.json();
-      setFormDraftData(data);
-      setIsLoading(false);
-    }
-  };
-  useEffect(() => {
-    updatePageData();
-  }, []);
 
   return (
     <Card style={{ height: "80vh" }}>
@@ -59,25 +35,10 @@ function GenerateSchedule() {
         </Nav.Item>
       </Nav>
 
-      {isLoading ? (
-        <div
-          className="d-flex justify-content-center align-items-center"
-          style={{ height: "100vh" }}
-        >
-          <Spinner animation="border" variant="primary" />
-        </div>
-      ) : (
-        <>
-          {tabNumber === 1 && (
-            <AllocateHoursContainer
-              fetchedDatesToHours={formDraftData["daily_study_hours"]}
-            />
-          )}
-          {tabNumber === 2 && <SelectPlans />}
-          {tabNumber === 3 && <ScheduleInformation />}
-          {tabNumber === 4 && <MakeAdjustments />}
-        </>
-      )}
+      {tabNumber === 1 && <AllocateHoursContainer />}
+      {tabNumber === 2 && <SelectPlans />}
+      {tabNumber === 3 && <ScheduleInformation />}
+      {tabNumber === 4 && <MakeAdjustments />}
     </Card>
   );
 }
--- frontend/src/pages/GenerateScheduleTab/containers/AllocateHoursContainer.jsx
+++ frontend/src/pages/GenerateScheduleTab/containers/AllocateHoursContainer.jsx
@@ -1,10 +1,15 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { Spinner } from "react-bootstrap";
 
 import {
   getISODatesToZeroHours,
   updateManyHours,
   updateSingleHours,
 } from "../dateFormat";
+import {
+  createFormDraft,
+  getFormDraft,
+} from "../../../services/scheduleRequests";
 import { updateFormDraft } from "../../../services/scheduleRequests";
 import AllocateHours from "../components/AllocateHours";
 
@@ -12,22 +17,43 @@ import AllocateHours from "../components/AllocateHours";
 const NUMBER_DAYS_TO_RENDER = 182; // Half a year
 const defaultDatesToHours = getISODatesToZeroHours(NUMBER_DAYS_TO_RENDER);
 
-function AllocateHoursContainer({ fetchedDatesToHours }) {
-  let initialDatesToHours;
-  if (fetchedDatesToHours) {
-    // Update the base hours with those saved on the server from the previous session
-    initialDatesToHours = updateManyHours(
-      defaultDatesToHours,
-      fetchedDatesToHours
-    );
-  } else {
-    // If the server does not have any data saved from the previous session then use
-    // the default of zero hours for each date
-    initialDatesToHours = defaultDatesToHours;
-  }
-
+function AllocateHoursContainer() {
+  const [isLoading, setIsLoading] = useState(true);
   const [weekNumber, setWeekNumber] = useState(0);
-  const [datesToHours, setDatesToHours] = useState(initialDatesToHours);
+  const [datesToHours, setDatesToHours] = useState(null);
+
+  const updatePageData = async () => {
+    let response = await getFormDraft();
+
+    // Create a ScheduleFormDraft instance if it does not already exist
+    if (response.status === 404) {
+      response = await createFormDraft();
+    }
+
+    if (response.ok) {
+      const data = await response.json();
+      const fetchedDatesToHours = data["daily_study_hours"];
+
+      let initialDatesToHours;
+      if (fetchedDatesToHours) {
+        // Update the default hours with those saved on the server from the previous session
+        initialDatesToHours = updateManyHours(
+          defaultDatesToHours,
+          fetchedDatesToHours
+        );
+      } else {
+        // If the server does not have any data saved from the previous session then use
+        // the default of zero hours for each date
+        initialDatesToHours = defaultDatesToHours;
+      }
+
+      setDatesToHours(initialDatesToHours);
+      setIsLoading(false);
+    }
+  };
+  useEffect(() => {
+    updatePageData();
+  }, []);
 
   // Event handling
   const handleWeekNumberDecrement = () => {
@@ -73,14 +99,25 @@ function AllocateHoursContainer({ fetchedDatesToHours }) {
   };
 
   return (
-    <AllocateHours
-      datesToHours={datesToHours}
-      weekNumber={weekNumber}
-      onWeekNumberDecrement={handleWeekNumberDecrement}
-      onWeekNumberIncrement={handleWeekNumberIncrement}
-      onHoursClick={handleHoursClick}
-      onZeroAllHours={handleZeroAllHours}
-    />
+    <>
+      {isLoading ? (
+        <div
+          className="d-flex justify-content-center align-items-center"
+          style={{ height: "100vh" }}
+        >
+          <Spinner animation="border" variant="primary" />
+        </div>
+      ) : (
+        <AllocateHours
+          datesToHours={datesToHours}
+          weekNumber={weekNumber}
+          onWeekNumberDecrement={handleWeekNumberDecrement}
+          onWeekNumberIncrement={handleWeekNumberIncrement}
+          onHoursClick={handleHoursClick}
+          onZeroAllHours={handleZeroAllHours}
+        />
+      )}
+    </>
   );
 }
 
