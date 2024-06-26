31e17c5c02df4cba83b8fce9b193083863a34147
arantruslove <125707012+arantruslove@users.noreply.github.com>
2024-06-26 17:35:10 +0100

    Removing unused function.

--- frontend/src/pages/GenerateScheduleTab/utils.js
+++ frontend/src/pages/GenerateScheduleTab/utils.js
@@ -1,14 +1,3 @@
-/**Add a field with a default value to each object in a list of objects. */
-export function addFieldToObjects(listToAddFieldsTo, fieldName, value) {
-  const updatedList = JSON.parse(JSON.stringify(listToAddFieldsTo)); // Deep copy
-
-  for (let i = 0; i < updatedList.length; i++) {
-    updatedList[i][fieldName] = value;
-  }
-
-  return updatedList;
-}
-
 /******************************************
  * Handling date and time utility functions
  ******************************************/
@@ -119,7 +108,7 @@ export function splitByWeek(inputDates) {
 }
 
 /******************************************
- * Handling date and time utility functions
+ * Handling plan data structure conversions
  ******************************************/
 
 /**Converts a list of plans data structure into a dictionary format where id is the key
