294065e238d1cde99a4f83eeeaff77531456fdd6
arantruslove <125707012+arantruslove@users.noreply.github.com>
2024-06-27 15:53:52 +0100

    Adding TopicHours boilerplate code.

--- frontend/src/pages/GenerateScheduleTab/components/TopicHours.jsx
+++ frontend/src/pages/GenerateScheduleTab/components/TopicHours.jsx
@@ -1,8 +1,40 @@
+import { Card, Accordion } from "react-bootstrap";
+
 function TopicHours() {
   return (
-    <>
-      <p>Make Adjustments</p>
-    </>
+    <Card.Body
+      style={{ display: "flex", flexDirection: "column", height: "80%" }}
+    >
+      <Card.Title className="mb-3">
+        Specify the Number of Hours to Spend on Each Topic
+      </Card.Title>
+      <Accordion defaultActiveKey="0">
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>Accordian Item #1 (1/5h) </Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="1">
+          <Accordion.Header>Accordion Item #2</Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+    </Card.Body>
   );
 }
 
