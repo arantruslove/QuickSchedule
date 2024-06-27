685503aee3100851886ace594a991a3f1e0879bb
arantruslove <125707012+arantruslove@users.noreply.github.com>
2024-06-27 18:47:21 +0100

    Optimising the view for speed.

--- backend/schedules/views.py
+++ backend/schedules/views.py
@@ -1,3 +1,4 @@
+from django.utils.dateparse import parse_date
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -13,37 +14,33 @@ from schedules.serializers import StudyDateHourSerializer
 @api_view(["POST", "PATCH"])
 @permission_classes([IsAuthenticated])
 def study_dates_hours_list_view(request):
-
     if request.method == "POST":
         study_dates_hours = StudyDateHour.objects.filter(user=request.user)
 
-        # Storing a map of the date representation to hour
-        dates_to_hours = {}
-        for study_date_hour in study_dates_hours:
-            date_str = study_date_hour.date.strftime("%Y-%m-%d")
-            dates_to_hours[date_str] = study_date_hour.hours
+        # Storing a map of the date representation to hours
+        dates_to_hours = {
+            study_date_hour.date.strftime("%Y-%m-%d"): study_date_hour.hours
+            for study_date_hour in study_dates_hours
+        }
 
         # Delete all the dates as they will be replaced with fresh ones
-        dates_to_delete = StudyDateHour.objects.filter(user=request.user)
-        dates_to_delete.delete()
+        StudyDateHour.objects.filter(user=request.user).delete()
 
-        # Create consecutive date intances for a set number of dates
+        # Create consecutive date instances for a set number of dates
         N_DAYS = 180  # Approximately half a year
         user_time_zone = request.user.time_zone
         dates_list = get_consecutive_dates(N_DAYS, user_time_zone)
-        for date in dates_list:
-            # Creating the instance and keeping the original hours field
-            try:
-                hours = dates_to_hours[date]
-            except:
-                hours = 0
-            data = {"user": request.user.id, "date": date, "hours": hours}
-            serializer = StudyDateHourSerializer(data=data)
-            if serializer.is_valid():
-                serializer.save()
-            else:
-                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-            study_date_hour.save()
+
+        new_instances = []
+        for date_str in dates_list:
+            hours = dates_to_hours.get(date_str, 0)
+            new_instance = StudyDateHour(
+                user=request.user, date=parse_date(date_str), hours=hours
+            )
+            new_instances.append(new_instance)
+
+        # Bulk create all new instances
+        StudyDateHour.objects.bulk_create(new_instances)
 
         # Return a list of all of the StudyDateHour instances
         instances = StudyDateHour.objects.filter(user=request.user)
