c4fa6e3accb61871dff33d3872901785aff75b7b
arantruslove <125707012+arantruslove@users.noreply.github.com>
2024-06-29 22:04:40 +0100

    Homepage now fetches topics and displays them on the calendar.

--- frontend/src/pages/HomeTab/components/Home.jsx
+++ frontend/src/pages/HomeTab/components/Home.jsx
@@ -2,23 +2,7 @@ import React from "react";
 import FullCalendar from "@fullcalendar/react";
 import listPlugin from "@fullcalendar/list";
 
-function Home() {
-  const events = [
-    {
-      title: "Event 1",
-      start: "2024-06-29",
-    },
-    {
-      title: "Event 2",
-      start: "2024-06-29",
-    },
-    {
-      title: "Event 3",
-      start: "2024-06-27",
-    },
-    // Add more events here
-  ];
-
+function Home({ events }) {
   return (
     <div style={{ height: "100%", width: "100%" }}>
       <FullCalendar
@@ -27,7 +11,7 @@ function Home() {
         height="100%"
         firstDay={1}
         events={events}
-        allDayText="" // Change the all-day text to your custom text
+        allDayText=""
       />
     </div>
   );
--- frontend/src/pages/HomeTab/containers/HomeContainer.jsx
+++ frontend/src/pages/HomeTab/containers/HomeContainer.jsx
@@ -1,7 +1,54 @@
+import { useState, useEffect } from "react";
+import { Spinner } from "react-bootstrap";
+
+import { getScheduleTopics } from "../../../services/scheduleRequests";
 import Home from "../components/Home";
 
+// Utility functions
+const formatTopicsToEvents = (scheduleTopics) => {
+  const events = [];
+  for (const topic of scheduleTopics) {
+    const event = {
+      title: `${topic["title"]} - ${topic["hours"]}h `,
+      date: topic["study_date"],
+    };
+    events.push(event);
+  }
+  return events;
+};
+
 function HomeContainer() {
-  return <Home />;
+  const [isLoading, setIsLoading] = useState(true);
+  const [events, setEvents] = useState(null);
+
+  const updatePageData = async () => {
+    const response = await getScheduleTopics();
+
+    if (response.ok) {
+      const scheduleTopics = await response.json();
+      const fetchedEvents = formatTopicsToEvents(scheduleTopics);
+      setEvents(fetchedEvents);
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    updatePageData();
+  }, []);
+  return (
+    <>
+      {isLoading ? (
+        <div
+          className="d-flex justify-content-center align-items-center"
+          style={{ height: "100vh" }}
+        >
+          <Spinner animation="border" variant="primary" />
+        </div>
+      ) : (
+        <Home events={events} />
+      )}
+    </>
+  );
 }
 
 export default HomeContainer;
